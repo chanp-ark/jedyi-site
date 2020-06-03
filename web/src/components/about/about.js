6a779dd557f9c9c72b41fba55ce1621d2583d2d2
chanp-ark <chanpark93@gmail.com>
2020-06-03 15:46:52 -0700

    fix layouts for message and about

--- web/src/components/about/about.js
+++ web/src/components/about/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import Container from '../../components/container'
 import Title from '../title'
 import './about.styles.css'
 
@@ -7,31 +8,28 @@ const JedImage = styled('div')`
   background-image: url('/images/jed-profile-about.jpg');
   background-position: top 10% center;
   background-size: cover;
-  height: 50vh;
-  width: 40vh;
-  @media (max-width: 900px) {
+  height: 61vh;
+  width: 55vh;
+  margin: 1rem;
+  @media (max-width: 950px) {
     margin: 0 1rem 1rem 1rem;
   }
 `
 
 const Biography = styled('div')`
-  height: 51vh;
-  width: 40vh;
-  overflow: scroll;
-
-  @media (max-width: 900px) {
-    margin-top: 2rem;
-  }
+  height: 61vh;
+  width: 55vh;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  margin: 1rem;
 `
 
 const AboutContainer = styled('div')`
   display: flex;
-  margin: 1rem;
-  justify-content: space-between;
+  justify-content: space-around;
 
-  @media (max-width: 900px) {
+  @media (max-width: 950px) {
     flex-wrap: wrap;
-    text-align: center;
     justify-content: space-around;
   }
 `
@@ -40,40 +38,41 @@ const AboutMe = () => {
   return (
     <>
       <Title>Jed Yi</Title>
-      <AboutContainer>
-        {/* image of jed */}
-        <JedImage />
-        {/* Biography, for now, hard coding it in. later, create schema for user to be able to update it himself */}
-        <Biography>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Tristique et egestas quis ipsum
-            suspendisse. Malesuada proin libero nunc consequat interdum. Volutpat est velit egestas
-            dui. Viverra nam libero justo laoreet sit amet cursus. Fringilla ut morbi tincidunt
-            augue interdum velit euismod in pellentesque. Suscipit adipiscing bibendum est ultricies
-            integer quis auctor elit. Enim facilisis gravida neque convallis a cras semper auctor.
-            Dolor purus non enim praesent elementum. Lectus quam id leo in vitae turpis massa sed
-            elementum. Consequat ac felis donec et odio pellentesque diam. Nunc mattis enim ut
-            tellus. Ipsum dolor sit amet consectetur adipiscing. Feugiat nisl pretium fusce id
-            velit. Venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam. A diam
-            sollicitudin tempor id eu nisl nunc mi. Elementum nibh tellus molestie nunc non blandit.
-            Tristique sollicitudin nibh sit amet commodo nulla facilisi. Diam quam nulla porttitor
-            massa id neque aliquam.
-          </div>
-          <div>
-            Eget lorem dolor sed viverra ipsum nunc aliquet. Magna ac placerat vestibulum lectus.
-            Tellus in metus vulputate eu scelerisque felis imperdiet proin. Eget mi proin sed libero
-            enim sed faucibus turpis in. Nunc lobortis mattis aliquam faucibus purus in. Quis enim
-            lobortis scelerisque fermentum dui faucibus. Vel quam elementum pulvinar etiam. Feugiat
-            nisl pretium fusce id velit. Aliquam sem et tortor consequat. Mollis nunc sed id semper
-            risus in hendrerit. Ultrices neque ornare aenean euismod elementum nisi. Aliquam
-            malesuada bibendum arcu vitae. Gravida dictum fusce ut placerat orci. Fermentum leo vel
-            orci porta non. Condimentum mattis pellentesque id nibh tortor id aliquet. Nulla
-            pellentesque dignissim enim sit amet. Potenti nullam ac tortor vitae purus faucibus
-            ornare suspendisse.
-          </div>
-        </Biography>
-      </AboutContainer>
+      <Container>
+        <AboutContainer>
+          <JedImage />
+          {/* Biography, for now, hard coding it in. later, create schema for user to be able to update it himself */}
+          <Biography>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Tristique et egestas quis ipsum
+              suspendisse. Malesuada proin libero nunc consequat interdum. Volutpat est velit egestas
+              dui. Viverra nam libero justo laoreet sit amet cursus. Fringilla ut morbi tincidunt
+              augue interdum velit euismod in pellentesque. Suscipit adipiscing bibendum est ultricies
+              integer quis auctor elit. Enim facilisis gravida neque convallis a cras semper auctor.
+              Dolor purus non enim praesent elementum. Lectus quam id leo in vitae turpis massa sed
+              elementum. Consequat ac felis donec et odio pellentesque diam. Nunc mattis enim ut
+              tellus. Ipsum dolor sit amet consectetur adipiscing. Feugiat nisl pretium fusce id
+              velit. Venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam. A diam
+              sollicitudin tempor id eu nisl nunc mi. Elementum nibh tellus molestie nunc non blandit.
+              Tristique sollicitudin nibh sit amet commodo nulla facilisi. Diam quam nulla porttitor
+              massa id neque aliquam.
+            </div>
+            <div>
+              Eget lorem dolor sed viverra ipsum nunc aliquet. Magna ac placerat vestibulum lectus.
+              Tellus in metus vulputate eu scelerisque felis imperdiet proin. Eget mi proin sed libero
+              enim sed faucibus turpis in. Nunc lobortis mattis aliquam faucibus purus in. Quis enim
+              lobortis scelerisque fermentum dui faucibus. Vel quam elementum pulvinar etiam. Feugiat
+              nisl pretium fusce id velit. Aliquam sem et tortor consequat. Mollis nunc sed id semper
+              risus in hendrerit. Ultrices neque ornare aenean euismod elementum nisi. Aliquam
+              malesuada bibendum arcu vitae. Gravida dictum fusce ut placerat orci. Fermentum leo vel
+              orci porta non. Condimentum mattis pellentesque id nibh tortor id aliquet. Nulla
+              pellentesque dignissim enim sit amet. Potenti nullam ac tortor vitae purus faucibus
+              ornare suspendisse.
+            </div>
+          </Biography>
+        </AboutContainer>
+      </Container>
     </>
   )
 }
--- web/src/components/sermon/sermon.js
+++ web/src/components/sermon/sermon.js
@@ -1,22 +1,47 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
+import Container from "../container"
 import Title from '../title'
 
 import './sermon.styles.css'
 
+const VideoContainer = styled('div')`
+  height: 50vh;
+  width: 100%;
+  margin: 2rem auto 0 auto;
+  padding-top: 3rem;
+  
+  @media(max-width:660px) {
+    height: 40vh;
+    width: 80%;
+  }
+`
+
+const LinkContainer = styled('div')`
+  margin: 1rem auto;
+  text-align: center;
+  font-style: italic;
+`
+
 const SermonVideo = () => {
   return (
-    <>
+    <Container>
       <Title>Message</Title>
-      <iframe
-        width='560'
-        height='315'
-        src='https://www.youtube.com/embed/0ZAHleKuPBw'
-        frameBorder='0'
-        allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-        allowFullScreen
-      />
-    </>
+      <VideoContainer>
+        <iframe
+          width='80%'
+          height='80%'
+          src='https://www.youtube.com/embed/0ZAHleKuPBw'
+          frameBorder='0'
+          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        />
+        <LinkContainer>
+          Hope in Troubled Times
+        </LinkContainer>
+      </VideoContainer>
+    </Container>
   )
 }
 
