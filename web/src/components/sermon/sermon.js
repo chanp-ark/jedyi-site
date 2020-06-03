9de3fdea558e23fe1ba33fa96ca7a1029cbbcb33
chanp-ark <chanpark93@gmail.com>
2020-06-03 16:15:06 -0700

    fix format for video to be smaller at large screen

--- web/src/components/sermon/sermon.js
+++ web/src/components/sermon/sermon.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-import Container from "../container"
 import Title from '../title'
 
 import './sermon.styles.css'
 
+const MessageContainer = styled('div')`
+  box-sizing: border-box;
+  max-width: 820px;
+  padding: 1.5em;
+  margin: 0 auto;
+
+  @media (--media-min-small) {
+    padding: 2em 0;
+  }
+`
+
 const VideoContainer = styled('div')`
   height: 50vh;
   width: 100%;
-  margin: 2rem auto 0 auto;
-  padding-top: 3rem;
+  margin: 2rem auto;
   
   @media(max-width:660px) {
-    height: 40vh;
-    width: 80%;
+    height: 35vh;
+    width: 100%;
+    margin: 2rem auto;
   }
 `
 
@@ -22,16 +32,17 @@ const LinkContainer = styled('div')`
   margin: 1rem auto;
   text-align: center;
   font-style: italic;
+  
 `
 
 const SermonVideo = () => {
   return (
-    <Container>
+    <MessageContainer>
       <Title>Message</Title>
       <VideoContainer>
         <iframe
-          width='80%'
-          height='80%'
+          width='100%'
+          height='100%'
           src='https://www.youtube.com/embed/0ZAHleKuPBw'
           frameBorder='0'
           allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
@@ -41,7 +52,7 @@ const SermonVideo = () => {
           Hope in Troubled Times
         </LinkContainer>
       </VideoContainer>
-    </Container>
+    </MessageContainer>
   )
 }
 
