c2359d96554bb28225e27b29176e514ebecf3a89
chanp-ark <chanpark93@gmail.com>
2020-07-06 12:43:55 -0700

    remove repeated category in desk

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -2,7 +2,7 @@ import S from '@sanity/desk-tool/structure-builder'
 import MdSettings from 'react-icons/lib/md/settings'
 
 const hiddenDocTypes = listItem =>
-  !['category', 'person', 'blog', 'event', 'siteSettings'].includes(listItem.getId())
+  !['category', 'person', 'blog', 'picture', 'event', 'siteSettings'].includes(listItem.getId())
 
 export default () =>
   S.list()
